809056a7c62dce26b22026dc7bec72f93336cc43
root <root@mx>
2021-01-17 16:08:19 +0700

    latest

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/', function () {
-    \Mail::to('abdullah.dicky.syarifuddin@gmail.com')
+    \Mail::to('wisatagowok42@gmail.com')
         ->send(new \App\Mail\PostMail('Mengirim Email Menggunakan Gmail SMTP Laravel 8', 'bdullz'));
     return 'Terkirim';
 });
